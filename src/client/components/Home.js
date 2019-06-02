a02bbbd98057db2369a515f128776366b8deec92
Ankit Agnihotri <ankit@Ankits-MacBook-Pro.local>
2019-06-03 00:26:43 +0530

    first commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>I'm the home best Component</div>
+      <button onClick={() => console.log('Hiiiii')}>Press me</button>
+    </div>
+  );
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import express from 'express';
+import renderer from './helpers/renderer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const app = express();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+app.use(express.static('public'));
+app.get('/', (req, res) => {
+  
+
+  res.send(renderer());
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
